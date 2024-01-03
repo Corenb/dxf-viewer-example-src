0ad339f906460fd718d33af73f848e1e2369872c
Corenb <coco_man@hotmail.fr>
2024-01-03 18:37:25 +0100

    Update deploy url to new repo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd dist || exit
+rm -rf .git
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:corenb/dxf-viewer-example-src.git master
+git push -f git@github.com:corenb/dxf-viewer-example.git master
